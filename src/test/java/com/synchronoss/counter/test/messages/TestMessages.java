36f929ff338d62b2077def1207e423059c836dad
gualtief <gualtief@hypatia.(none)>
2015-11-09 20:04:13 +0000

    Formatting

--- src/test/java/com/synchronoss/counter/test/messages/TestMessages.java
+++ src/test/java/com/synchronoss/counter/test/messages/TestMessages.java
@@ -6,7 +6,8 @@ import com.synchronoss.counter.test.constants.TestConstants;
 public class TestMessages {
 
     public static final String INVALID_OPERATION_EXCEPTION_MESSAGE =
-            String.format(ExceptionMessages.INVALID_OPERATION_MESSAGE_FORMAT, TestConstants.INVALID_OPERATION_INPUT_STRING);
+            String.format(ExceptionMessages.INVALID_OPERATION_MESSAGE_FORMAT,
+                    TestConstants.INVALID_OPERATION_INPUT_STRING);
     public static final String INVALID_MODE_EXCEPTION_MESSAGE =
             String.format(ExceptionMessages.INVALID_MODE_MESSAGE_FORMAT, TestConstants.INVALID_MODE_INPUT_STRING);
 
